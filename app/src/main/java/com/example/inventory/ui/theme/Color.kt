c3ae983ea4fe795d2b6d2831883a8c84db1d3e27
JSD-2511-01009\yamauchi.ks2_jsdata <yamauchi.ks2@jsdata.co.jp>
2026-05-12 15:45:14 +0900

    色変更

--- app/src/main/java/com/example/inventory/ui/theme/Color.kt
+++ app/src/main/java/com/example/inventory/ui/theme/Color.kt
@@ -17,10 +17,8 @@ package com.example.inventory.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_primary = Color(0xFF86AF91)   //元0xFF6B3DD4
+val md_theme_light_primary = Color(0xFF86AF91) //元0xFF6B3DD4
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-val md_theme_light_primaryContainer = Color(0xFFE9DDFF)
-val md_theme_light_onPrimaryContainer = Color(0xFF22005D)
 val md_theme_light_secondary = Color(0xFF625B71)
 val md_theme_light_onSecondary = Color(0xFFFFFFFF)
 val md_theme_light_secondaryContainer = Color(0xFFE8DEF8)
--- app/src/main/java/com/example/inventory/ui/theme/Theme.kt
+++ app/src/main/java/com/example/inventory/ui/theme/Theme.kt
@@ -33,8 +33,6 @@ import androidx.core.view.WindowCompat
 private val LightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
     onPrimary = md_theme_light_onPrimary,
-    primaryContainer = md_theme_light_primaryContainer,
-    onPrimaryContainer = md_theme_light_onPrimaryContainer,
     secondary = md_theme_light_secondary,
     onSecondary = md_theme_light_onSecondary,
     secondaryContainer = md_theme_light_secondaryContainer,
